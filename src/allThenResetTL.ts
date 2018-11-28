1d397132c374f0d5a577449075209cc3b9070929
kenokabe <stken2050@gmail.com>
2018-11-29 00:08:21 +0900

    Update

--- src/allThenResetTL.ts
+++ src/allThenResetTL.ts
@@ -1,5 +1,5 @@
 import { T } from
-  "./timeline-monad";
+  "./timeline-monad.js";
 
 interface timeline {
   type: string;
@@ -7,34 +7,50 @@ interface timeline {
   sync: Function;
 }
 
-const allThenResetTL = (TLs: timeline[]) => T(
-  (self: timeline) => {
-    const updateFlagsTL = T(
-      (self: timeline) =>
-        self.now = Array(TLs.length).fill(0)
-    );
-    const resetFlag = () => {
-      updateFlagsTL.now = Array(TLs.length).fill(0)
-      return true;
-    };
-    const replace = (arr: number[]) => (index: number) =>
-      (val: number) =>
-        [...arr.slice(0, index), val,
-        ...arr.slice(index + 1)];
+const right = (a: any) => (b: any) => b;
 
-    const updateFlag = TLs
-      .map((TL, index) =>
-        TL.sync(() => (updateFlagsTL.now =
-          replace(updateFlagsTL.now as number[])(index)(1))));
-    const allUpdatedCheck = updateFlagsTL.sync(
-      (updateFlags: number[]) =>
-        (updateFlags         //all  updated
-          .reduce((a: number, b: number) => (a * b)) === 1)
-          ? resetFlag() &&
-          (self.now = TLs.map((TL) => TL.now))
-          : true
+const replace = (arr: number[]) =>
+  (index: number) =>
+    (val: number) =>
+      [...arr.slice(0, index), val,
+      ...arr.slice(index + 1)];
+
+const updateFlagsTL = (TLs: timeline[]) =>
+  (selfAll: timeline) =>
+    T((self: timeline) => {
+      self.now = Array(TLs.length).fill(0);
+
+      TLs
+        .map((TL, index1) =>
+          TL.sync(
+            () => right
+
+              (TLs.map((tl, index0) =>
+                (tl.now === undefined)
+                  ? self.now = replace(self.now)(index0)(0)
+                  : undefined))
+
+              (self.now = replace(self.now)(index1)(1))
+          )
+        );
+
+      self.sync(
+        (updateFlags: number[]) =>
+          selfAll.now =
+          (updateFlags //all  updated--------
+            .reduce((a: number, b: number) =>
+              (a * b)) !== 1)
+            ? undefined//no trigger
+            : right
+              (self.now = Array(TLs.length).fill(0))
+              (TLs.map((TL) => TL.now))
+      );
+    });
+
+const allThenResetTL =
+  (TLs: timeline[]) =>
+    T((selfAll: timeline) =>
+      updateFlagsTL(TLs)(selfAll)
     );
-  }
-);
 
 export { allThenResetTL };
